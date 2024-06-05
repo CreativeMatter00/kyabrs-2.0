12359f099178ba7888a7ebbd97cb29d04d0db5b2
GolamMuid <golammuid@gmail.com>
2024-06-05 17:06:02 +0600

    added participate page

---
+++ src/components/participate/Difference.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Difference = () => {
+	return (
+		<div className="container mx-auto p-6">
+			<div>
+				<div className="mb-4">
+					<p className="text-2xl text-brandPrimaryLs3"> Make a Difference: </p>
+					<p className="text-2xl"> Participate in Clinical Trials </p>
+				</div>
+				<p>
+					Volunteering for clinical trials is crucial to advancing medical
+					research and developing new treatments. On this page, you’ll discover
+					the importance of clinical trials, gain insight into the participant
+					experience, and find out how you can contribute to improving
+					healthcare for everyone.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Difference;
