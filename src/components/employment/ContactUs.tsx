bab035342a668ff1180a8056aa4ca2a8dd91e9b3
GolamMuid <golammuid@gmail.com>
2024-06-08 18:30:52 +0600

    updated employment

--- src/components/employment/ContactUs.tsx
+++ src/components/employment/ContactUs.tsx
@@ -6,19 +6,19 @@ const ContactUs = () => {
 		<div className="text-bgPrimary">
 			<p className="font-bold text-[#FF6A5E] mb-4">Contact Us</p>
 
-			<div className="flex items-center gap-8">
-				<p className="">
+			<div className="flex items-center flex-col md:flex-row gap-8">
+				<p className="basis-1/4">
 					For any inquiries regarding careers or volunteer opportunities at
 					KYA-BRS, please contact our HR department{" "}
 				</p>
 
-				<div className="flex flex-col gap-8 h-full">
+				<div className="flex flex-col gap-8 h-full basis-1/4">
 					<div className="bg-brandColorDs1 rounded-md p-6 flex flex-col items-center gap-4">
 						<MdOutlineEmail className="text-brandPrimary text-3xl" />
 						<p className="text-lg text-center ">info@kyabrs.org</p>
 					</div>
 				</div>
-				<div className="flex flex-col gap-8 h-full">
+				<div className="flex flex-col gap-8 h-full basis-1/4">
 					<div className="bg-brandColorDs1 rounded-md p-6 flex flex-col items-center gap-4">
 						<LuPhone className="text-brandPrimary text-3xl" />
 						<p className="text-lg text-center ">+880 1733-070041</p>
