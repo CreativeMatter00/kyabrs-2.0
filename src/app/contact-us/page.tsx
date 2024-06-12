6f42e91d59aa2a03fade924960f769a74ffd955d
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2024-06-12 12:56:45 +0600

    Contact top and bottom fixed

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -10,7 +10,7 @@ const DynamicContactUs = dynamic(
 
 const page = () => {
   return (
-    <div className="pt-20 lg:pt-28 bg-brandSecondary">
+    <div className="pt-20 lg:pt-28 bg-brandSecondary pb-12">
       <DynamicContactUs />
     </div>
   );
--- src/components/contactUs/ContactUs.tsx
+++ src/components/contactUs/ContactUs.tsx
@@ -18,10 +18,10 @@ const ContactUs = () => {
         </div>
       </div>
 
-      <div className="py-6 text-bgPrimary">
+      <div className="pt-6 text-bgPrimary">
         <div className="container overflow-hidden px-4 mx-auto">
           <div className="flex flex-col lg:flex-row gap-4">
-            <div className="w-full lg:w-[20%]">
+            <div className="w-full lg:basis-1/4">
               <div className="flex flex-row lg:flex-col gap-8 flex-wrap">
                 <div className="flex flex-col gap-8 h-full">
                   <Link
@@ -57,7 +57,7 @@ const ContactUs = () => {
               </div>
             </div>
 
-            <div className="w-full lg:w-[80%] h-[400px] lg:h-auto">
+            <div className="w-full lg:basis-3/4 h-[400px] lg:h-auto rounded-lg overflow-hidden">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14553.720884644978!2d89.7027804!3d24.2267272!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fde0a604c0a259%3A0xc3f1901ce95b20ad!2sKhwaja%20Yunus%20Ali%20Medical%20College%20%26%20Hospital!5e0!3m2!1sen!2sbd!4v1711511768719!5m2!1sen!2sbd"
                 width="100%"
@@ -70,68 +70,6 @@ const ContactUs = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className="grid grid-cols-1 md:grid-cols-2">
-				<div className="flex flex-col gap-8 p-8">
-					<div className="grid grid-cols-2 gap-8">
-						<div className="flex flex-col items-center justify-start gap-4">
-							<div className="w-20 h-20 rounded-full flex items-center justify-center border-2 text-2xl">
-								<IoLocationOutline />
-							</div>
-
-							<p className="text-xl font-medium uppercase text-textPrimary">
-								Address:
-							</p>
-							<p className="text-lg text-center text-textSecondary">
-								Enayetpur,Sirajganj <br /> Bangladesh
-							</p>
-						</div>
-
-						<div className="flex flex-col items-center justify-start gap-4">
-							<div className="w-20 h-20 rounded-full flex items-center justify-center border-2 text-2xl">
-								<MdOutlineEmail />
-							</div>
-
-							<p className="text-xl font-medium uppercase text-textPrimary">
-								Email:
-							</p>
-							<Link
-								className="text-lg text-center text-textSecondary hover:text-textPrimary hover:underline "
-								href="mailto: info@kyabrs.org"
-							>
-								info@kyabrs.org
-							</Link>
-						</div>
-					</div>
-
-					<div className="flex flex-col items-center justify-center gap-4">
-						<div className="w-20 h-20 rounded-full flex items-center justify-center border-2 text-2xl">
-							<LuPhone />
-						</div>
-
-						<p className="text-xl font-medium uppercase text-textPrimary">
-							Call us:
-						</p>
-						<Link
-							className="text-lg text-center text-textSecondary hover:text-textPrimary hover:underline "
-							href="tel: +8801733070041"
-						>
-							+880 1733-070041
-						</Link>
-					</div>
-				</div>
-
-				<div className="min-h-[450px]">
-					<iframe
-						src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14553.720884644978!2d89.7027804!3d24.2267272!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fde0a604c0a259%3A0xc3f1901ce95b20ad!2sKhwaja%20Yunus%20Ali%20Medical%20College%20%26%20Hospital!5e0!3m2!1sen!2sbd!4v1711511768719!5m2!1sen!2sbd"
-						width="100%"
-						height="100%"
-						allowFullScreen={true}
-						loading="lazy"
-						style={{ border: 0 }}
-					></iframe>
-				</div>
-			</div> */}
     </div>
   );
 };
