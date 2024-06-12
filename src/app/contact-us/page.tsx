f253e9f8a00c45e098b120cc0069adf382e23e57
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2024-06-12 12:33:18 +0600

    adout page top and bottom fixed

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,20 +1,19 @@
-import Banner from "@/components/ui/banner/Banner";
 import Loader from "@/components/ui/loader/Loader";
 import dynamic from "next/dynamic";
 
 const DynamicContactUs = dynamic(
-	() => import("@/components/contactUs/ContactUs"),
-	{
-		loading: () => <Loader />,
-	}
+  () => import("@/components/contactUs/ContactUs"),
+  {
+    loading: () => <Loader />,
+  }
 );
 
 const page = () => {
-	return (
-		<div className="pt-24 lg:pt-32 bg-brandSecondary">
-			<DynamicContactUs />
-		</div>
-	);
+  return (
+    <div className="pt-20 lg:pt-28 bg-brandSecondary">
+      <DynamicContactUs />
+    </div>
+  );
 };
 
 export default page;
--- src/components/aboutUs/WhoWeAre.tsx
+++ src/components/aboutUs/WhoWeAre.tsx
@@ -1,35 +1,35 @@
 function WhoWeAre() {
-	return (
-		<div className="text-bgPrimary px-4">
-			<div className="pb-6">
-				<p className="text-2xl">
-					<span className="text-brandPrimaryLs3"> Trusted </span> Bio-Research
-					Partner
-				</p>
-			</div>
+  return (
+    <div className="text-bgPrimary pt-4">
+      <div className="pb-6">
+        <p className="text-2xl">
+          <span className="text-brandPrimaryLs3"> Trusted </span> Bio-Research
+          Partner
+        </p>
+      </div>
 
-			<div className="text-bgPrimary text-base font-medium">
-				<p>
-					The first pioneering contract research company in Bangladesh offering
-					clinical and non-clinical research services is Khwaja Yunus Ali
-					Medical College & Hospital Bio Research Services (KYAMCH-BRS).
-					Additionally, it is the only hospital-affiliated bio-research
-					institution in the nation, allowing it to conduct both screening and
-					clinical trials.
-					<br />
-					<br />
-					Our area of expertise is clinical trials. Our over 40,000 square feet
-					clinical trial facility includes a well-designed screening space and a
-					highly developed bioanalytical laboratory. Phase I–III clinical trials
-					as well as bioequivalence studies can be conducted by KYANCH-BRS.
-					KYANCH-BRS conducts a number of clinical trials under the watchful eye
-					of international regulatory bodies. A team of committed specialists
-					oversees the operation. Regulations currently in effect are followed
-					in all operations.
-				</p>
-			</div>
-		</div>
-	);
+      <div className="text-bgPrimary text-base font-medium">
+        <p>
+          The first pioneering contract research company in Bangladesh offering
+          clinical and non-clinical research services is Khwaja Yunus Ali
+          Medical College & Hospital Bio Research Services (KYAMCH-BRS).
+          Additionally, it is the only hospital-affiliated bio-research
+          institution in the nation, allowing it to conduct both screening and
+          clinical trials.
+          <br />
+          <br />
+          Our area of expertise is clinical trials. Our over 40,000 square feet
+          clinical trial facility includes a well-designed screening space and a
+          highly developed bioanalytical laboratory. Phase I–III clinical trials
+          as well as bioequivalence studies can be conducted by KYANCH-BRS.
+          KYANCH-BRS conducts a number of clinical trials under the watchful eye
+          of international regulatory bodies. A team of committed specialists
+          oversees the operation. Regulations currently in effect are followed
+          in all operations.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default WhoWeAre;
